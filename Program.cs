6e8f4e1475d0f4e17a098a798cfaaf81a4542376
David Tellander <david@meridium.se>
2019-04-22 16:29:07 +0200

    Switch expressions
    Refactored `ResponseTimeResult` to two different types implementing the
    same interface.
    
    - `ResponseTimeResult` now represents a successful response
    - `ResponseError` now represents a failed response
    
    Using a switch expression to match the result and format the result
    message.

--- Program.cs
+++ Program.cs
@@ -11,22 +11,22 @@ using System.IO;
 namespace CSharp8Demo {
     class Program {
         static async Task Main(string[] args) {
-            using var urlSource = IsInputRedirected 
-                ? In 
-                : new StreamReader(args[0]); 
+            using var urlSource = IsInputRedirected
+                ? In
+                : new StreamReader(args[0]);
 
-            await foreach (var siteurl in GetSiteUrls(urlSource)) { 
+            await foreach (var siteurl in GetSiteUrls(urlSource)) {
                 WriteLine(
-                    TimingMessage(
+                    FormatResponse(
                         await GetTimedResponse(siteurl)));
             }
         }
 
         async static IAsyncEnumerable<string> GetSiteUrls(TextReader urlSource) {
             string siteurl;
-            while(true) {
+            while (true) {
                 siteurl = await urlSource.ReadLineAsync();
-                if(siteurl != null) {
+                if (siteurl != null) {
                     yield return siteurl;
                 }
                 else {
@@ -35,68 +35,75 @@ namespace CSharp8Demo {
             }
         }
 
-        static async Task<ResponseTimeResult> GetTimedResponse(string url) {
+        static async Task<IResponseResult> GetTimedResponse(string url) {
             using var http = new HttpClient();
-            ResponseTimeResult result;
+            IResponseResult result;
             try {
                 Stopwatch watch = Stopwatch.StartNew();
                 var response = await http.GetAsync(url);
-                result = ResponseTimeResult.Response(
+                result = new ResponseTimeResult(
                     url,
                     (int)response.StatusCode,
                     watch.ElapsedMilliseconds
                 );
             }
             catch (Exception e) {
-                result = ResponseTimeResult.Error(url, e);
+                result = new ResponseError(url, e);
             }
             return result;
         }
 
-        static string TimingMessage(ResponseTimeResult result) {
-            if(result.Exception != null) {
-                return $"x {result.Url} Request failed miserably: {result.Exception.Message}"
-                    .Pastel(Color.Red);
-            } 
-
-            if(result.StatusCode == 200) {
-                switch(result.ResponseTime) {
-                    case var ms when ms < 300:
-                        return $"- {result.Url} Excellent response time: {ms}"
-                            .Pastel(Color.LightBlue);
-                    case var ms when ms < 1000:
-                        return $"- {result.Url} Good response time: {ms}"
-                            .Pastel(Color.YellowGreen);
-                    case var ms when ms < 3000:
-                        return $"- {result.Url} Decent response time: {ms}"
-                            .Pastel(Color.Yellow);
-                    case var ms when ms >= 3000:
-                        return $"- {result.Url} Bad response time: {ms}"
-                            .Pastel(Color.Orange);
-                }
-            } else if (result.StatusCode == 404) {
-                return $"- {result.Url} Not found url".Pastel(Color.DarkOrange);
-            } else if (result.StatusCode == 500) {
-                return $"- {result.Url} Server error".Pastel(Color.DarkOrange);
-            }
-
-            return "Whut?";
-        }
+        static string FormatResponse(IResponseResult result) =>
+            result switch {
+                ResponseTimeResult(var url, 200, var ms) when ms < 300 =>
+                    $"- {url} - Excellent response time, {ms} ms"
+                        .Pastel(Color.LightBlue),
+                ResponseTimeResult(var url, 200, var ms) when ms < 800 =>
+                    $"- {url} - Good response time, {ms} ms"
+                        .Pastel(Color.ForestGreen),
+                ResponseTimeResult(var url, 200, var ms) when ms < 2000 =>
+                    $"- {url} - Ok response time, {ms} ms"
+                        .Pastel(Color.Yellow),
+                ResponseTimeResult(var url, 200, var ms) when ms >= 2000 =>
+                    $"- {url} - Bad response time, {ms} ms"
+                        .Pastel(Color.Orange),
+                ResponseTimeResult(var url, 404, _) =>
+                    $"- {url} - Not found"
+                        .Pastel(Color.Red),
+                ResponseTimeResult(var url, var status, _) when status >= 500 =>
+                    $"- {url} - Server error ({status})"
+                        .Pastel(Color.Red),
+                ResponseError(var url, var ex) =>
+                    $"x {url} - An exception occurred: {ex.Message}"
+                        .Pastel(Color.DarkRed),
+                IResponseResult r => $"? {r.Url} - Unknown response..."
+            };
     }
 
-    readonly struct ResponseTimeResult {
+    interface IResponseResult {
+        string Url { get; }
+    }
+
+    readonly struct ResponseError : IResponseResult {
+        public string Url { get; }
+        public Exception Exception { get; }
+
+        public ResponseError(string url, Exception e) =>
+            (Url, Exception) = (url, e);
+
+        public void Deconstruct(out string url, out Exception e) =>
+            (url, e) = (Url, Exception);
+    }
+
+    readonly struct ResponseTimeResult : IResponseResult {
         public string Url { get; }
         public int StatusCode { get; }
         public long ResponseTime { get; }
-        public Exception? Exception { get; }
 
-        public ResponseTimeResult(string url, int status, long ms, Exception? error) =>
-            (Url, StatusCode, ResponseTime, Exception) = (url, status, ms, error);
+        public ResponseTimeResult(string url, int status, long ms ) =>
+            (Url, StatusCode, ResponseTime) = (url, status, ms);
 
-        public static ResponseTimeResult Response(string url, int status, long ms) =>
-            new ResponseTimeResult(url, status, ms, null);
-
-        public static ResponseTimeResult Error(string url, Exception error) =>
-            new ResponseTimeResult(url, 0, 0, error);
+        public void Deconstruct(out string url, out int status, out long ms) =>
+            (url, status, ms) = (Url, StatusCode, ResponseTime);
     }
 }
